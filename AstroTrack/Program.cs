78c55e70f6437aa0c250819676dfb06523a29ff1
Arthur Correia Delila <arthur.cdelila@gmail.com>
2026-06-08 17:36:12 -0300

    Configuração de porta para deploy no render

--- AstroTrack/Program.cs
+++ AstroTrack/Program.cs
@@ -140,6 +140,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Run($"http://0.0.0.0:{port}");
 
 public partial class Program { }
